bba1d5a54e448e1ee1feca005fa13bdadad5953b
Carlos Ducrós <ducroscarlospablo@gmail.com>
2023-10-15 17:38:25 -0300

    Actualizado espera simulada

--- src/utils/esperaSimulada.js
+++ src/utils/esperaSimulada.js
@@ -1,3 +1,3 @@
 export const esperaSimulada = async (tiempo) => {
-  return await new Promise((resolve) => setTimeout(() => resolve("Exito"), 0))
+  return await new Promise((resolve) => setTimeout(() => resolve("Exito"), tiempo))
 }
